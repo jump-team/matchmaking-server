f4b40b6f80af49112d444aca9d46f7538b86aa0d
PrettierCI <commits@prettierci.com>
2018-11-27 17:49:59 +0000

    Sync with Prettier

--- src/match.ts
+++ src/match.ts
@@ -1,14 +1,15 @@
 class Match {
-  private averageRank: number
+  private averageRank: number;
   constructor(public readonly players: number) {
-    return
+    return;
   }
-  public isGoodMatch (rank: number, range: number) { // Checks if a match is good for the specific rank
-    let lowest: number = this.averageRank - range
-    let highest: number = this.averageRank + range
-    if (rank >= lowest && rank <= highest) { // Match works
-      
+  public isGoodMatch(rank: number, range: number) {
+    // Checks if a match is good for the specific rank
+    let lowest: number = this.averageRank - range;
+    let highest: number = this.averageRank + range;
+    if (rank >= lowest && rank <= highest) {
+      // Match works
     }
   }
 }
-export { Match }
+export { Match };
