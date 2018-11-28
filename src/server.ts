63e466927ce8c7cac76ddbdec2900f7ebcb818ff
Glitch (matchmaking-server) <none>
2018-11-28 17:53:23 +0000

    🍽🌒 Updated with Glitch

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,17 @@ import * as express from "express";
 const app = express();
 import { Match } from "./match";
 import { FailReason, MatchCheckReturnObject } from "./MatchCheckReturnObject";
+import * as path from "path";
+import * as expressHandlebars from "express-handlebars";
 let matches: Match[] = [];
+
+app.set("views", path.join(__dirname, "../views"));
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
 
 app.get("/", function(request: Request, response: Response) {
   response.status(200);
-  response.sendFile("/app/views/index.html");
+  response.render("index");
 });
 
 app.get("/ping", function(request: Request, response: Response) {
