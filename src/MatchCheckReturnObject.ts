cecda124f7a3fcece8ab1c5481fc1dff662a7854
Glitch (matchmaking-server) <none>
2018-11-28 13:08:57 +0000

    Redo exports for MatchCheckReturnObject and FailReason

--- src/MatchCheckReturnObject.ts
+++ src/MatchCheckReturnObject.ts
@@ -1,14 +1,12 @@
-enum FailReason {
+export enum FailReason {
   RankLow,
   RankHigh,
   Other
 }
-export FailReason as FailReason;
-class ReturnObject {
+export class MatchCheckReturnObject {
   constructor(
     public readonly success: boolean,
     public readonly matchId: string,
     reason?: FailReason = FailReason.Other
   );
 }
-export ReturnObject as MatchCheckReturnObject;
