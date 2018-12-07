779c1f9c5fd16b72f082c0a00abdb1fb387ee060
PrettierCI <commits@prettierci.com>
2018-12-07 17:31:07 +0000

    Sync with Prettier

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,12 @@ import * as expressHandlebars from "express-handlebars";
 import * as bodyParser from "body-parser";
 import { Player } from "./player";
 let matchTypes: object = { Standard: [], Pro: [], Mega: [] };
-const ranges: object = { Standard: 250, Pro: 250, Mega: 500 }
+const ranges: object = { Standard: 250, Pro: 250, Mega: 500 };
 
 app.use(bodyParser.json());
 app.set("views", path.join(__dirname, "../views"));
-app.engine('handlebars', expressHandlebars());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", expressHandlebars());
+app.set("view engine", "handlebars");
 
 app.get("/", function(request: Request, response: Response) {
   response.status(200);
@@ -27,17 +27,21 @@ app.get("/ping", function(request: Request, response: Response) {
 app.post("/:type/findmatch", function(request: Request, response: Response) {
   const info = request.body;
   if (matchTypes[request.params.type]) {
-    const matches = matchTypes[request.params.type]
+    const matches = matchTypes[request.params.type];
     let bestMatch;
     for (let v of matches) {
-      const returnObject = v.isGoodMatch(info.rank, ranges[request.params.type]);
+      const returnObject = v.isGoodMatch(
+        info.rank,
+        ranges[request.params.type]
+      );
       if (returnObject.success === true) {
-        bestMatch = matchTypes[request.params.type][returnObject.matchId];7
+        bestMatch = matchTypes[request.params.type][returnObject.matchId];
+        7;
         break;
       }
     }
     if (!bestMatch instanceof Match) {
-      res.status(201).json({ match_found: false, retry: true })
+      res.status(201).json({ match_found: false, retry: true });
     }
   } else {
     res.status(404).json({ match_found: false, retry: true });
