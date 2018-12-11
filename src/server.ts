3a73b90768fa006769e5f0bc8c68b9ab8ad3cb80
Glitch (matchmaking-server) <none>
2018-12-11 17:38:47 +0000

    👨‍👨‍👦☔️ Updated with Glitch

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,13 @@ import * as path from "path";
 import * as expressHandlebars from "express-handlebars";
 import * as bodyParser from "body-parser";
 import { Player } from "./player";
-let matchTypes: object = { Standard: [], Pro: [], Mega: [] };
-const ranges: object = { Standard: 250, Pro: 250, Mega: 500 };
+let matchTypes: object = { Standard: {}, Pro: {}, Mega: {} };
+const ranges: object = { Standard: 250, Pro: 250, Mega: 500 }
 
 app.use(bodyParser.json());
 app.set("views", path.join(__dirname, "../views"));
-app.engine("handlebars", expressHandlebars());
-app.set("view engine", "handlebars");
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
 
 app.get("/", function(request: Request, response: Response) {
   response.status(200);
@@ -27,24 +27,32 @@ app.get("/ping", function(request: Request, response: Response) {
 app.post("/:type/findmatch", function(request: Request, response: Response) {
   const info = request.body;
   if (matchTypes[request.params.type]) {
-    const matches = matchTypes[request.params.type];
+    const matches = matchTypes[request.params.type]
     let bestMatch;
     for (let v of matches) {
-      const returnObject = v.isGoodMatch(
-        info.rank,
-        ranges[request.params.type]
-      );
+      const returnObject = v.isGoodMatch(info.rank, ranges[request.params.type]);
       if (returnObject.success === true) {
-        bestMatch = matchTypes[request.params.type][returnObject.matchId];
-        7;
+        bestMatch = matchTypes[request.params.type][returnObject.matchId];7
         break;
       }
     }
     if (!bestMatch instanceof Match) {
-      res.status(201).json({ match_found: false, retry: true });
+      response.status(201).json({ match_found: false, retry: true })
+    } else {
+      response.status(200).json(bestMatch);
     }
   } else {
-    res.status(404).json({ match_found: false, retry: true });
+    response.status(404).json({ match_found: false, retry: true });
+  }
+});
+
+app.get("/:type/:id" function(request: Request, response: Response) {
+  const info = request.body;
+  if (matchTypes[request.params.type]) {
+    const matches = matchTypes[request.params.type]
+    if (matches[request.params.id]) {
+      response.status(200).json(matches[request.params.id]);
+    }
   }
 });
 
